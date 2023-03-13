3482b27fc4b75a6663a3718d1e1ae0b95e533572
ZanderOl <olexandruk@gmail.com>
2023-03-13 13:19:42 +0200

    add close modal options
    add close options like overlay mouse click and escape button click

--- src/Modal.js
+++ src/Modal.js
@@ -1,18 +1,41 @@
-const Modal = ({ open, setOpen, children }) => (
-  <div className={`overlay animated ${open ? "show" : ""}`}>
-    <div className="modal">
-      <svg
-        onClick={() => setOpen(false)}
-        height="200"
-        viewBox="0 0 200 200"
-        width="200"
-      >
-        <title />
-        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-      </svg>
-      {children}
+import { useEffect } from "react";
+
+const Modal = ({ open, setOpen, children }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === "Escape") {
+        setOpen(false);
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [setOpen]);
+
+  const handleClick = (event) => {
+    if (event.target === event.currentTarget) {
+      setOpen(false);
+    }
+  };
+
+  return (
+    <div
+      className={`overlay animated ${open ? "show" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="modal">
+        <svg
+          onClick={() => setOpen(false)}
+          height="200"
+          viewBox="0 0 200 200"
+          width="200"
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
